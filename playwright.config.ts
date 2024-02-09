e27de7776f0fb0e4a0b8d61672db19ffb001cd20
Albert <albertn@blendyd.com>
2024-02-09 20:47:51 +0800

    feb 9

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,9 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 5000000,
+  expect: {
+    timeout: 10 * 50000,
+  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
